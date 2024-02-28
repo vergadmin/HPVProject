6dcddee47816c1f6623e3a959eade01d752ababc
rashi <rashighosh@ufl.edu>
2024-02-28 13:24:25 -0500

    fixed initial db logging issues

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,33 +1,33 @@
 window.addEventListener("load", () => {
-    // console.log("SAVING SESSION INFO LOCALLY")
-    // console.log(document.URL)
-
     let browserInfo = ""
     let dateTime = ""
     // var type = document.URL.split('/').reverse()[0]
-    var id = document.URL.split('/').reverse()[1]
+    var id = document.URL.split('/').reverse()[0]
+    console.log(id)
     sessionStorage.setItem("id", id)
     // sessionStorage.setItem("type", type)
 
     // (B1) PARSE USER AGENT
-    browserInfo = navigator.userAgent;
-    // console.log(browserInfo)
+    browserInfo = detectBrowser();
+    osInfo = getOS()
+    let platformInfo = checkDeviceType();
 
     // console.log("TIME")
     dateTime = new Date().toLocaleString() + " " + Intl.DateTimeFormat().resolvedOptions().timeZone;
     // console.log(dateTime)
-    sendGeneralData(browserInfo, dateTime)
+    sendGeneralData(browserInfo, osInfo, platformInfo, dateTime)
 
     
 });
 
-async function sendGeneralData(browserInfo, dateTime) {
-    // console.log("IN SEND TO SERVER GENERAL DATA")
-
+async function sendGeneralData(browserInfo, osInfo, platformInfo, dateTime) {
+    console.log("IN SEND TO SERVER GENERAL DATA")
     let url = '/updateDatabase';
     let data = {
-        'Date-time': dateTime,
+        'Date_time': dateTime,
         'Browser_Info': browserInfo,
+        'OS_Info': osInfo,
+        'Platform': platformInfo
     };
 
     let res = await fetch(url, {
@@ -39,10 +39,113 @@ async function sendGeneralData(browserInfo, dateTime) {
     });
     if (res.ok) {
         let ret = await res.json();
+        console.log("done sending to server")
         return JSON.parse(ret.data);
-
     } else {
         return `HTTP error: ${res.status}`;
     }
 }
 
+// helper function to detect browser
+function detectBrowser() {
+    var nAgt = navigator.userAgent;
+    var browserName  = navigator.appName;
+    var fullVersion  = ''+parseFloat(navigator.appVersion); 
+    var majorVersion = parseInt(navigator.appVersion,10);
+    var nameOffset,verOffset,ix;
+
+    // In Opera, the true version is after "OPR" or after "Version"
+    if ((verOffset=nAgt.indexOf("OPR"))!=-1) {
+        browserName = "Opera";
+        fullVersion = nAgt.substring(verOffset+4);
+        if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+        fullVersion = nAgt.substring(verOffset+8);
+    }
+    // In MS Edge, the true version is after "Edg" in userAgent
+    else if ((verOffset=nAgt.indexOf("Edg"))!=-1) {
+        browserName = "Microsoft Edge";
+        fullVersion = nAgt.substring(verOffset+4);
+    }
+    // In MSIE, the true version is after "MSIE" in userAgent
+    else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
+        browserName = "Microsoft Internet Explorer";
+        fullVersion = nAgt.substring(verOffset+5);
+    }
+    // In Chrome, the true version is after "Chrome" 
+    else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
+        browserName = "Chrome";
+        fullVersion = nAgt.substring(verOffset+7);
+    }
+    // In Safari, the true version is after "Safari" or after "Version" 
+    else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+        browserName = "Safari";
+        fullVersion = nAgt.substring(verOffset+7);
+        if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+        fullVersion = nAgt.substring(verOffset+8);
+    }
+    // In Firefox, the true version is after "Firefox" 
+    else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
+        browserName = "Firefox";
+        fullVersion = nAgt.substring(verOffset+8);
+    }
+    // In most other browsers, "name/version" is at the end of userAgent 
+    else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
+            (verOffset=nAgt.lastIndexOf('/')) ) 
+    {
+        browserName = nAgt.substring(nameOffset,verOffset);
+        fullVersion = nAgt.substring(verOffset+1);
+        if (browserName.toLowerCase()==browserName.toUpperCase()) {
+        browserName = navigator.appName;
+        }
+    }
+    // trim the fullVersion string at semicolon/space if present
+    if ((ix=fullVersion.indexOf(";"))!=-1)
+        fullVersion=fullVersion.substring(0,ix);
+    if ((ix=fullVersion.indexOf(" "))!=-1)
+        fullVersion=fullVersion.substring(0,ix);
+
+    majorVersion = parseInt(''+fullVersion,10);
+    if (isNaN(majorVersion)) {
+    fullVersion  = ''+parseFloat(navigator.appVersion); 
+    majorVersion = parseInt(navigator.appVersion,10);
+    }
+
+    return browserName + " " + majorVersion
+}
+
+// helper function to detect platform/os
+function getOS() {
+    const userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+    let os = null;
+  
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+    } else if (/Linux/.test(platform)) {
+      os = 'Linux';
+    }
+  
+    return os;
+}
+
+// helper function to detect if device is a mobile, tablet, or desktop based on screen width
+function checkDeviceType() {
+    const screenWidth = window.innerWidth;
+    
+    if (screenWidth < 768) {
+        return "Mobile";
+    } else if (screenWidth < 1024) {
+        return "Tablet";
+    } else {
+        return "Desktop";
+    }
+}
--- server.js
+++ server.js
@@ -13,18 +13,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 var sql = require("mssql");
 
-var vh = ''
-var type = ''
-var visitNum = 0;
-
-const columnNames = ['diseases', 'synonym1', 'synonym2', 'synonym3', 'synonym4', 'synonym5', 'synonym6', 'synonym7', 'synonym8', 'synonym9', 'synonym10', 'synonym11', 'synonym12', 'synonym13', 'synonym14', 'synonym15', 'synonym16', 'synonym17', 'synonym18', 'synonym19', 'synonym20', 'synonym21', 'synonym22', 'synonym23', 'synonym24', 'synonym25', 'synonym26', 'synonym27', 'synonym28', 'synonym29', 'synonym30', 'synonym31', 'synonym32', 'synonym33', 'synonym34', 'synonym35', 'synonym36', 'synonym37', 'synonym38', 'synonym39', 'synonym40', 'synonym41', 'synonym42', 'synonym43', 'synonym44', 'synonym45', 'synonym46', 'synonym47', 'synonym48', 'synonym49', 'synonym50', 'synonym51'];
-const vhTypes = ["bfe", "bme", "wfe", "wme", "hfe", "hme", "hfs", "hms"];
-// Modify based on Miriam/Emma's Qualtrics:
-const orderOfInfo =  ["I", "G", "E", "R"];
-const columnsInAG = 369
-const columnsInHO = 305
-const columnsInPZ = 355
-
 var userInfo = []
 
 const config = {
@@ -55,164 +43,75 @@ app.use(session({
     }
 }))
 
+// this function is called from the client to register info from the front end to the db
 app.post('/updateDatabase', async (req, res) => {
     let setList = ''
-    console.log("INSIDE UPDATE DATABASE")
+    console.log("IN UPDATE DATABASE")
     console.log(userInfo)
     console.log(req.body);
     for (const [key, value] of Object.entries(req.body)) {
-        // if (key==="VHType") {
-        //     vh = value
-        // }
         setList += key + `='` + value + `', `
     }
     setList = setList.slice(0, -2); 
-    // console.log(setList);
+    console.log(setList);
 
     // BEGIN DATABSAE STUFF:SENDING VERSION (R24 OR U01) AND ID TO DATABASE
     sql.connect(config, function (err) {
-    
         if (err) console.log(err);
-
         // create Request object
         var request = new sql.Request();
-
-        let queryString = `
-        UPDATE HPVDataTable
-        SET ` + setList + 
-        ` WHERE ID = '` + userInfo.ID + `' 
-        AND visitNum = '` + userInfo.visitNum + `'`;
+        let queryString = `UPDATE HPVDataTable SET ` + setList + ` WHERE Participant_ID = '` + userInfo.ID +  `'`;
         request.query(queryString, function (err, recordset) {
             if (err) console.log(err) 
-
-            res.send("Updated.");
+            console.log("updated db! with js index info")
         }); 
-    
     });
     // END DATABASE STUFF
 })
 
-app.post("/:id/:type/RetrieveConditions", (req, res) => {
-    let searchValue = (Object.entries(req.body)[0][1])
-    // console.log(searchValue);
-    const columnConditions = columnNames.map(column => `${column} LIKE '%${searchValue}%'`).join(' OR ')
-    // const finalConditions = columnConditions.slice(0, -3);
-    let queryString = `
-    SELECT TOP 10 diseases FROM Diseases
-    WHERE ${columnConditions}
-    `;
-
-    // console.log(queryString)
-
-    sql.connect(config, function (err) {
-        if (err) console.log(err)
-
-        var request = new sql.Request();
-        request.query(queryString, function (err, recordset) {
-            if (err) console.log(err)
-            // send records as a response
-            // console.log(recordset.recordset[0]);
-            let conditions = recordset.recordset;
-              // Sort items based on similarity score (higher score means more similar)
-            const sortedItems = conditions.sort((a, b) => stringSimilarity.compareTwoStrings(searchValue, b.diseases) - stringSimilarity.compareTwoStrings(searchValue, a.diseases));
-              
-              // Get the top 10 most similar items
-            res.json(sortedItems);    
-        }); 
-    })
-})
-
-app.get('/:id', checkPreviousVisit, addVisitToDatabase, (req, res) => {
+app.get('/:id', registerVisit, (req, res) => {
     id = req.params.id
     res.render('pages/index',{id: id})
 })
 
-
-
-
-function checkPreviousVisit(req, res, next) {
+function registerVisit(req, res, next) {
     sql.connect(config, function (err) {
         var request = new sql.Request();
-        console.log("CHECK PREVIOUS VISIT")
-        console.log(req.params.id);
+        console.log("IN REGISTER VISIT")
+        userInfo['ID'] = req.params.id
         // Query Check for Existing Entry In Table
-        let checkString = `SELECT visitNum FROM HPVDataTable WHERE Participant_ID = '` + req.params.id + `';`;
+        let checkString = `SELECT visitNum FROM HPVDataTable WHERE Participant_ID = '` + userInfo['ID'] + `';`;
+        console.log(checkString)
         request.query(checkString, function(err, recordset) {
             if (err) console.log(err);
-            console.log("CHECKING RECORDSET")
-            console.log(recordset.recordset.length)
+            // if user id exists in db, increment their visitNum
             if (recordset.recordset.length !== 0) {
-                console.log(recordset.recordset);
-                visitNum = recordset.recordset[0].visitNum + 1;
+                console.log("returning user");
+                sql.connect(config, function (err) {
+                    var request = new sql.Request();
+                    let queryString = `UPDATE HPVDataTable SET visitNum = visitNum+1 WHERE Participant_ID = '` + userInfo['ID'] + `';`;
+                    request.query(queryString, function (err, recordset) {
+                        if (err) console.log(err)
+                        console.log(recordset)
+                        next();
+                    })
+                })
             }
-            console.log("afterElse")
-            console.log(visitNum);
-            userInfo['visitNum'] = visitNum;
-            next();
+            // if user id is not in db, create new record
+            else {
+                console.log("first visit, adding to db")
+                sql.connect(config, function (err) {
+                    var request = new sql.Request();
+                    let queryString = `INSERT INTO HPVDataTable (Participant_ID) VALUES ('` + userInfo['ID'] + `')`;
+                    request.query(queryString, function (err, recordset) {
+                        if (err) console.log(err)
+                        next();
+                    })
+                })
+            }
         })
     })
 
-}
-
-function addVisitToDatabase(req, res, next) {
-    id = req.params.id
-    if (!userInfo["ID"])
-        userInfo["ID"] = id;
-    // After first time, we mark visitedIndex as true, and then we don't want to do it again.
-    if (visitNum !== 0) {
-        sql.connect(config, function (err) {
-            var request = new sql.Request();
-            let queryString = `UPDATE HPVDataTable SET visitNum = visitNum+1 WHERE Participant_ID=` + userInfo.ID + `;`;
-            // console.log(queryString);
-            request.query(queryString, function (err, recordset) {
-                if (err) console.log(err)
-                console.log("HAS VISITED")
-            })
-        })
-        next();
-        return;
-    } else {
-        sql.connect(config, function (err) {
-            var request = new sql.Request();
-            let queryString = `INSERT INTO HPVDataTable (Participant_ID, visitNum) VALUES ('` + userInfo.ID + `',` + userInfo.visitNum + `);`;
-            // console.log(queryString);
-            request.query(queryString, function (err, recordset) {
-                if (err) console.log(err)
-                console.log("HAS NOT VISITED")
-            })
-        })
-    }
-    next()
-}
-
-
-// Potentially Deprecated
-function setVHType(req, res, next) {
-    if (req.session.visitedIndex) {
-        next();
-        return;
-    }
-    if (userInfo.P ===  'Text') {
-        userInfo['VHType'] = 'text';
-    }
-    else {
-        // Change Black/White to contains Black/White potentially (based on answers for Survey Item)
-        if (userInfo.G === 'Female' && userInfo.R ==='Black') {
-            userInfo['VHType'] = 'bfe'
-        }
-        else if (userInfo.G === 'Male' && userInfo.R ==='Black') {
-            userInfo['VHType'] = 'bme'
-        }
-        else if (userInfo.G === 'Female' && userInfo.R ==='White') {
-            userInfo['VHType'] = 'wfe'
-        }
-        else if (userInfo.G === 'Male' && userInfo.R ==='White') {
-            userInfo['VHType'] = 'wme'
-        } else {
-            userInfo['VHType'] = 'bfe'
-        }
-    }
-    next()
 }
 
 // Virtual Human Types
@@ -223,7 +122,5 @@ app.use('/:id/EducationalComponent', function(req,res,next) {
     req.userInfo = userInfo
     next();
 }, EducationalComponentRouter)
-
-
 
 app.listen(process.env.PORT || 3000);
