cc25db82fd3e7072ce96cf2a75962e3b55f5bb4b
roshanv77 <rvenkatakrishnan@ufl.edu>
2024-03-07 14:55:24 -0500

    Added database code for questions and character selected
    Added database code for questions and character selected; Needs database code for durations

--- public/scripts/buttonClickHandler.js
+++ public/scripts/buttonClickHandler.js
@@ -1,4 +1,4 @@
-function logAndRedirect(pageName, response, id, type, nextPageURL){
+function buttonRedirect(id, type, nextPageURL){
     //todo log page name and resaponse to database
     //alert("PAGE NAME IS : ", pageName)
     window.location.href=`/${id}/EducationalComponent/${type}/${nextPageURL}`;
